8cf6bc7bf528b57c783224b61bda67f69ed3e965
KairuRengu <hyle200@gmail.com>
2015-07-27 16:34:05 -0400

    Server Render
    Index.ejs

--- server.js
+++ server.js
@@ -27,10 +27,8 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 
 	// ejs render automatically looks in the views folder
-	res.render('content-top-navigation');
-	res.render('top-navigation');
 	res.render('index');
-	res.render('footer');
+
 });
 
 app.post('/', function(req, res){
