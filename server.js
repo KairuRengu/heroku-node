8f8fec9fa25e47c02ed2f00d4826b1b1d73d349b
KairuRengu <hyle200@gmail.com>
2015-07-26 23:30:12 -0400

    Not giving up

--- server.js
+++ server.js
@@ -52,3 +52,28 @@ app.get('/navigation', function(req, res){
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
 });
+
+function checkAuth(req, res, next){
+	if(!req.session.user_id){
+		res.send('You are not authirzed to view this page.');
+	}else{
+		next();
+	}
+}
+app.get('/my_secret_page', checkAuth, function (req, res){
+	res.send('If you are viewing this page it means you are logged in');
+});
+
+app.post('/login', function (req, res){
+	var post = req.body;
+	if( post.user == 'john' && post.password == 'johnspassword'){
+		req.session.user_id = johns_usre_id_here;
+		res.redirect('/my_secret_page');
+	}else{
+		res.send('Bad user/pass');
+	}
+});
+app.get('/logout', function (req,res){
+	delete req.session.user_id;
+	res.redirect('/login');
+});
