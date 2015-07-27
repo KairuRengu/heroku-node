180569a8d88b40e64af2ff23ca2e3730b4148c05
KairuRengu <hyle200@gmail.com>
2015-07-26 22:39:20 -0400

    Revert "Reversing"
    This reverts commit dec88cb469d1772ae3fcf44834b05eb6d10a5df3.

--- server.js
+++ server.js
@@ -1,96 +1,21 @@
+// vendor libraries
 var express = require('express');
-var mysql = require('mysql');
-var util = require('util');
-var TwitterStrategy = require('passport-twitter').Strategy
-var session = require('express-session');
-
-var passport = require('passport');
-var flash = require('connect-flash');
-var morgan = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var bcrypt = require('bcrypt-nodejs');
+var ejs = require('ejs');
+var path = require('path');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
-var config = require('configuration/config');
+// custom libraries
+// routes
+var route = require('./route');
+// model
+var Model = require('./model');
 
-//----------------------------------------Database Connection Example---------------------
-var connection = mysql.createConnection({
-	host	: 'hopper.wlu.ca',
-	user	: 'cram7290',
-	password : 'bigtop6',
-	database : 'cram7290'
-});
-if(config.use_database === "true"){
-	connection.connect();
-}
-passport.serializeUser(function(user,done){
-	done(null,user);
-});
-passport.deserializeUser(function(obj, done){
-	done(null,obj);
-});
-// Use the TwitterStrategy within Passport.
-
-passport.use(new TwitterStrategy({
-consumerKey: config.twitter_api_key,
-consumerSecret:config.twitter_api_secret ,
-callbackURL: config.callback_url
-},
-function(token, tokenSecret, profile, done) {
-process.nextTick(function () {
-//Check whether the User exists or not using profile.id
-if(config.use_database===’true’)
-{
-//Perform MySQL operations.
-});
-}
-return done(null, profile);
-});
-}
-));
-
-app.set(‘views’, __dirname + ‘/views’);
-app.set(‘view engine’, ‘ejs’);
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({ secret: ‘keyboard cat’, key: ‘sid’}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static(__dirname + ‘/public’));
-
-app.get(‘/’, function(req, res){
-res.render(‘index’, { user: req.user });
-});
-
-app.get(‘/account’, ensureAuthenticated, function(req, res){
-res.render(‘account’, { user: req.user });
-});
-
-app.get(‘/auth/twitter’, passport.authenticate(‘twitter’));
-
-app.get(‘/auth/twitter/callback’,
-passport.authenticate(‘twitter’, { successRedirect : ‘/’, failureRedirect: ‘/login’ }),
-function(req, res) {
-res.redirect(‘/’);
-});
-
-app.get(‘/logout’, function(req, res){
-req.logout();
-res.redirect(‘/’);
-});
-
-function ensureAuthenticated(req, res, next) {
-if (req.isAuthenticated()) { return next(); }
-res.redirect(‘/login’)
-}
 var app = express();
-
-var server = app.listen(port);
-var server = http.createServer(app);
-
-
-
-
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -102,8 +27,6 @@ app.set('view engine', 'ejs');
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
-
-//-----------------------------------------------------Page Routers ----------------------------
 // set the home page route
 app.get('/', function(req, res) {
 
@@ -131,7 +54,70 @@ app.get('/navigation', function(req, res){
 	res.render('navigation');
 });
 
-//------------------------------------------------- Open Port ------------------------------------
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
+});
+passport.use(new LocalStrategy(function(username, password, done) {
+   new Model.User({username: username}).fetch().then(function(data) {
+      var user = data;
+      if(user === null) {
+         return done(null, false, {message: 'Invalid username or password'});
+      } else {
+         user = data.toJSON();
+         if(!bcrypt.compareSync(password, user.password)) {
+            return done(null, false, {message: 'Invalid username or password'});
+         } else {
+            return done(null, user);
+         }
+      }
+   });
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.username);
+});
+
+passport.deserializeUser(function(username, done) {
+   new Model.User({username: username}).fetch().then(function(user) {
+      done(null, user);
+   });
+});
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(session({secret: 'secret strategic xxzzz code'}));
+app.use(passport.initialize());
+app.use(passport.session());
+// GET
+app.get('/', route.index);
+
+// signin
+// GET
+app.get('/signin', route.signIn);
+// POST
+app.post('/signin', route.signInPost);
+
+// signup
+// GET
+app.get('/signup', route.signUp);
+// POST
+app.post('/signup', route.signUpPost);
+
+// logout
+// GET
+app.get('/signout', route.signOut);
+
+/********************************/
+
+/********************************/
+// 404 not found
+app.use(route.notFound404);
+var server = app.listen(app.get('port'), function(err) {
+   if(err) throw err;
+
+   var message = 'Server is running @ http://localhost:' + server.address().port;
+   console.log(message);
 });
