415fefeb7a74af10270906d124baf67c462b9a0f
KairuRengu <hyle200@gmail.com>
2015-07-27 04:44:03 -0400

    Another Attempt

--- server.js
+++ server.js
@@ -53,8 +53,8 @@ app.post('/navigation',function(req,res){
 	var userEmail = req.body.UsernameEmail;
 	var userPassword = req.body.UsernamePassword;
 	var html = 'Hello: ' + userEmail + '.<br>' + userPassword;
-	res.writeHeader(200, {"Content-Type": "text/html"});
 	res.write(html);
+	res.write("Where does this go?");
 	res.end();
 });
 app.get('/login', function(req,res){
